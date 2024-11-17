68bb61d943da1ece53b3fe6e72d4e7ef4721971f
atloo1 <atlorenson@gmail.com>
2024-11-16 17:48:14 -0800

    v1.0.0 (#1)
    * Update .gitignore
    
    * working code
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ make a travel pinboard
 
 ## prerequisites
 - [poetry](https://python-poetry.org/docs/#installing-with-pipx)
+- `./input/map.png` [downloaded](https://drive.google.com/drive/folders/1dQKogx8fkdZk7-pvOS2jz5OdCEysO1km) from the [8K-BAM project](https://www.alternatehistory.com/forum/threads/the-xk-bam-map-series.441440/)
+- `./input/pinboard.tsv` with fields `x`, `y`, & `0` where pins point at `(x,y)` with heading `0`°
 
 ```
 git clone https://github.com/atloo1/travel-pinboard.git
@@ -25,6 +27,10 @@ pyenv local 3.9   # or your choice
 poetry install
 poetry run pre-commit install
 ```
+### adding pins (spreadsheet formulas for a 8192x4096 equirectangular map)
+`x`:  `=(longitude+180)*(8192/360)`
+
+`y`: `=(90-latitude)*(4096/180)`
 ### test locally (preemptively pass the corresponding GitHub action)
 ```
 poetry run pytest
--- travel_pinboard/main.py
+++ travel_pinboard/main.py
@@ -1,5 +1,75 @@
+from numbers import Number
+from typing import Tuple, Union
+
+import pandas as pd
+from PIL import Image
+
+
+def get_tip_details_set():
+    """parse spreadsheet for pins' (x, y, θ)"""
+    df = pd.read_csv('input/pinboard.tsv', sep='\t', na_values='-')
+    cols = ['x', 'y', 'θ']
+    mask = df[cols].notna().all(axis=1)
+    df = df[cols].loc[mask].astype(int)
+    tip_details_set = set(df.itertuples(index=False, name=None))
+    return tip_details_set
+
+
+def heading_to_theta(heading: Union[float, int]) -> Union[float, int]:
+    """
+    convert compass heading (°) to an equivalent for PIL.Image.rotate
+
+    equivalent to piecewise
+    f(θ) = -θ + 90 [0 ≤ θ ≤ 90] &
+    f(θ) = -θ + 450 [90 < θ ≤ 360]
+    """
+    if not (0 <= heading <= 360):
+        raise ValueError(f'{heading}° not in range [0, 360]')
+    if heading <= 90:
+        heading += 360
+    theta = 450 - heading
+    return theta
+
+
+def get_coordinates(tip_x: int, tip_y: int, heading: Number) -> Tuple[int, int]:
+    """
+    convert tip coordinates to an equivalent for PIL.Image.paste
+
+    hard coded for 11x26 arrow b/c 45° rotation resizes image, complicating math
+    """
+    top_left_x = tip_x
+    top_left_y = tip_y
+    if heading == 45:
+        top_left_x -= 23
+        top_left_y -= 4
+    elif heading == 135:
+        top_left_x -= 23
+        top_left_y -= 22
+    elif heading == 225:
+        top_left_x -= 5
+        top_left_y -= 22
+    elif heading == 315:
+        top_left_x -= 5
+        top_left_y -= 4
+    else:
+        raise NotImplementedError(f'{heading}° rotation not implemented')
+    return top_left_x, top_left_y
+
+
 def main():
-    raise NotImplementedError
+    """make & save pinboard"""
+    map = Image.open('input/map.png')
+    pin = Image.open('input/pin.png')
+    tip_details_set = get_tip_details_set()
+
+    for tip_x, tip_y, heading in tip_details_set:
+        theta = heading_to_theta(heading)
+        rotated_pin = pin.rotate(theta, resample=Image.BICUBIC, expand=True)
+        coordinates = get_coordinates(tip_x, tip_y, heading)
+        map.paste(rotated_pin, coordinates, rotated_pin)
+
+    map.save('output/pinboard.png')
+    map.show()
 
 
 if __name__ == '__main__':
